4009b1887e8ae0dfaffc5cb6a6ff24fb1d363e7d
TudorSante <tudorsantejudean.com>
2023-09-15 19:35:15 +0300

    Routing overview exercise

--- app/components/AddToCart.tsx
+++ app/components/AddToCart.tsx
@@ -3,7 +3,11 @@ import React from "react";
 
 /* This comp will be rendered on the client side - CSR */
 const AddToCart = () => {
-  return <button onClick={() => console.log("Clicked")}>ClickMe</button>;
+  return (
+    <button className="btn btn-primary" onClick={() => console.log("Clicked")}>
+      ClickMe
+    </button>
+  );
 };
 
 export default AddToCart;
