9d91cd99f6af35603a6c8883b689f416070aa744
FIXED-TERM Santejudean Tudor (PS-EC/EDS3) <fixed-term.Tudor.Santejudean@ro.bosch.com>
2023-09-22 18:13:17 +0300

    Using PUT to update user objs

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -11,3 +11,22 @@ export function GET(request: NextRequest, { params: { id } }: Props) {
     return NextResponse.json({ error: "User not found" }, { status: 404 });
   return NextResponse.json({ id: 1, name: "Mosh" });
 }
+
+export async function PUT(request: NextRequest, { params: { id } }: Props) {
+  // Validate the request body
+  const body = await request.json();
+  // If invalid, return 400 - bad req
+  if (!body.name)
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+  // Fetch the user with the given id
+  // If doesn't exists, return 404 - not found
+  if (id > 10)
+    return NextResponse.json(
+      { error: "User not existent in DB." },
+      { status: 404 }
+    );
+
+  // Update the user
+  // Return the updated user
+  return NextResponse.json({ id: 1, name: body.name });
+}
